775bf0d2fc89edc455a2881bc693650fce764a01
[dyad] <juansaucortes@gmail.com>
2026-06-21 02:56:34 -0600

    [dyad] Corregido error de importación de useMemo en CardReconciliationDialog - wrote 1 file(s)

--- src/components/CardReconciliationDialog.tsx
+++ src/components/CardReconciliationDialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
